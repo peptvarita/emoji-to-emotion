8582435449fe51f94999453afe496bd049db9986
peptvarita <154039630+peptvarita@users.noreply.github.com>
2024-02-11 11:43:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+Webcam.set({
+    width : 350,
+    height :300,
+    image_format : 'png',
+    png_quality : 90
+})
+camera = document.getElementById("camera")
+Webcam.attach("#camera")
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri) {
+        document.getElementById("outcome").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'
+      })
+}
+
+console.log('ml5 version:', ml5.version)
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/_NXl0nbqq/model.json',modelLoaded)
+
+function modelLoaded() {
+    console.log('Model Loaded!')
+}
+
+function speak(){
+    var synth = window.speechSynthesis
+    speak_data_1 = "The First Prediction is "+prediction1;
+    speak_data_2 = "And the second Prediction is "+prediction2;
+var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2)
+synth.speak(utterThis);
+}
+
+function identifyimage(){
+    img = document.getElementById('captured_image')
+    classifier.classify(img, gotResult);
+}
+function gotResult(error ,results){
+    if (error){
+        console.error(error)
+    }
+else{
+    console.log(results)
+    document.getElementById('result_emotion_name').innerHTML = results[0].label
+    document.getElementById('result_emotion_name2').innerHTML = results[1].label
+prediction1 = results[0].label
+prediction2 = results[1].label
+speak()
+if(results[0].label == "Happy")
+{
+    document.getElementById("update_emoji").innerHTML = "&#128522;"
+}
+if(results[0].label == "Surprised")
+{
+    document.getElementById("update_emoji").innerHTML = "&#128558;"
+}
+if(results[0].label == "bored")
+{
+    document.getElementById("update_emoji").innerHTML = "&#128529;"
+}
+
+
+if(results[1].label == "Happy")
+{
+    document.getElementById("update_emoji2").innerHTML = "&#128522;"
+}
+if(results[1].label == "Surprised")
+{
+    document.getElementById("update_emoji2").innerHTML = "&#128559;"
+}
+if(results[1].label == "bored")
+{
+    document.getElementById("update_emoji2").innerHTML = "&#128530;"
+}
+
+}
+
+    
+}
+
+
+
+
+
+
+
+
+
+
